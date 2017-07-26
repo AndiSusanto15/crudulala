f19347e83f64bf38f631c50af9d9b8ce2d56bb98
Andi Susanto <susanto.andi11@gmail.com>
2017-07-26 13:17:36 +0700

    tambah fitur datatables biar asik

--- tampilsiswa.php
+++ tampilsiswa.php
@@ -10,11 +10,12 @@
 	<head>
 		<title>Data Siswa</title>
 		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
 	</head>
 	<body>
 		<div class="container">
 			<h2 class="text-center">Data Siswa SD Luar Binasa</h2><hr>
-			<table class="table table-bordered table-striped">
+			<table class="table table-bordered table-striped" id="tabelnya">
 				<thead class="text-center">
 					<tr class="bg-primary">
 						<th class="text-center">Foto</th>
@@ -41,5 +42,14 @@
 			</table>
 			<a href="tambahsiswa.php" class="btn btn-primary">Tambah Data</a>
 		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+		<script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+		<script>
+			$(document).ready(function() {
+				$('#tabelnya').DataTable();
+			} );
+		</script>
 	</body>
 	</html>
